e519c68afd89ae60c2aa98afdedc9c5fb1da130a
Kianna L Moore <kianna.lm@gmail.com>
2016-11-28 16:26:57 -0600

    DOM Assignments

---
+++ blank.js
@@ -0,0 +1,7 @@
+
+var paragraphChange = document.querySelector('.change-me');
+
+paragraphChange.addEventListener('click', function(){
+    paragraphChange.textContent = 'How about green eggs and ham, its holiday healthy! Lol!';
+    console.log('It is GOOOODD!');
+});
