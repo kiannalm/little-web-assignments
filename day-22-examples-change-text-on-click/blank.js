53fd13def82bfdb2aceeaf19f9668323c1bb8b7b
Kianna L Moore <kianna.lm@gmail.com>
2016-12-02 17:22:51 -0600

    DOM Click Meister

---
+++ day-22-examples-change-text-on-click/blank.js
@@ -0,0 +1,7 @@
+
+var theParagraph = document.querySelector('.the-paragraph');
+
+theParagraph.addEventListener('click', function() {
+    theParagraph.textContent = 'Werk, werk, werk, werky, werky, werk, werk!!!';
+    console.log("Yesssssss!!!!");
+});
